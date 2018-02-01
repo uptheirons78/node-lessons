72d67bdf5ca12e7de695051d436d7d7fb0eb4811
Mauro Bono <bono.mauro@gmail.com>
2018-02-01 10:20:32 +0100

    add body-parser and use  urlencodedParser and jsonParser

--- express/express-app/app.js
+++ express/express-app/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
 
 /****MIDDLEWARE TO USE PUBLIC DIR****/
 app.use('/assets', express.static(__dirname + '/public'));
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +17,17 @@ app.get('/', (req, res) => {
 
 app.get('/person/:id', (req, res) => {
     res.render('person', { ID: req.params.id });
+});
+
+app.post('/person', urlencodedParser, (req, res) => {
+    res.send('Thank You!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+app.post('/personjson', jsonParser, (req, res) => {
+    res.send('Thank You for the JSON data!');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
 });
 
 app.get('/api', (req, res) => {
