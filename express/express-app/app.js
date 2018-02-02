179403dc1e6e7ba122fe973ac33ca8ba13d79faa
Mauro Bono <bono.mauro@gmail.com>
2018-02-02 10:10:27 +0100

    add some new features

--- express/express-app/app.js
+++ express/express-app/app.js
@@ -8,8 +8,8 @@ const htmlController = require('./controllers/htmlController');
 /****MIDDLEWARE TO USE PUBLIC DIR****/
 app.use('/assets', express.static(__dirname + '/public'));
 
-
 app.set('view engine', 'ejs');
+
 app.use('/', (req, res, next) => {
     console.log("Request Url: " + req.url);
     next();
