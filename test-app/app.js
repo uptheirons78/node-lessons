32b85d8951fd5ae41983e80533cd11b5eff2076d
Mauro Bono <bono.mauro@gmail.com>
2018-02-02 12:40:55 +0100

    add another little app with MEAN stack structure and funtionalities

--- test-app/app.js
+++ test-app/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const people = [
+    { name: 'John Doe' },
+    { name: 'Jane Doe' },
+    { name: 'Jim Doe' }
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {serverPeople: people});
 });
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
