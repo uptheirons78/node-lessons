25a715b47a5e63bd1146319841d65143389fd54d
Mauro Bono <bono.mauro@gmail.com>
2018-02-02 11:45:10 +0100

    add a basic MEAN stack app

---
+++ test-app/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
---
+++ test-app/public/js/app.js
@@ -0,0 +1,13 @@
+angular.module('TestApp', []);
+
+angular.module('TestApp')
+    .controller('MainController', ctrlFunc);
+
+function ctrlFunc() {
+    this.message = 'Hello';
+    this.people = [
+        {name: 'John Doe'},
+        {name: 'Jane Doe'},
+        {name: 'Jim Doe'}
+    ];
+}
